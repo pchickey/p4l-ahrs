82bbf9b5e8e4f21d93362b2182c242dca4e2cdb2
Pat Hickey <pat@moreproductive.org>
2010-02-15 17:20:13 -0500

    two stubs for ITG3200 i2c calls

---
+++ ITG_3200_i2c.h
@@ -0,0 +1,11 @@
+// Invensense ITG-3200 I2C interface
+// Pat Hickey - pat@moreproductive.org
+// 15 Feb 2010
+
+#ifndef __ITG_3200_I2C_H
+#define __ITG_3200_I2C_H
+
+// placeholder for now
+
+
+#endif
---
+++ ITG_3200_i2c.pde
@@ -0,0 +1,11 @@
+// Invensense ITG-3200 I2C interface
+// Pat Hickey - pat@moreproductive.org
+// 15 Feb 2010
+
+#include "ITG_3200_hw.h"
+#include "ITG_3200_i2c.h"
+
+
+// placeholder for now
+
+
